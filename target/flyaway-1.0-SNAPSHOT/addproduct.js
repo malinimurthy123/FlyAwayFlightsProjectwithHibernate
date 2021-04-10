f5df9e7837794587f5abe36c05a18081dacc4221
“malinimurthy123git config --global user.name “malinimurthy123 <krmalini77@gmail.comgit config --global user.email krmalini77@gmail.com>
2021-04-10 02:04:47 -0700

    Few more bugs fixed

---
+++ target/flyaway-1.0-SNAPSHOT/addproduct.js
@@ -0,0 +1,9 @@
+/**
+ * Addproduct.js Javascript
+ */
+
+function addpet_onclick() {
+    alert("Inside function JS");
+    var x = document.getElementsByClassName("bookingconfirmation");
+    x[0].submit(); //form submission
+}
